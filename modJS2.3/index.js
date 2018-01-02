918d31e5319d37de6e8b14ae7a64d932bae07f98
Nathaniel Adam <grizmoore95@gmail.com>
2018-01-02 05:54:19 -0500

    changed things

---
+++ modJS2.3/index.js
@@ -0,0 +1,10 @@
+"use strict"
+//strict uses the modern JS modifications
+alert("welp this will be affected by strict");
+/*
+It's good practice to use strict in your code in order to use the most recent modifications
+
+You cannot take the strict away, there is no "stop strict" code available
+
+Strict must be used at the top of the JS code
+*/
