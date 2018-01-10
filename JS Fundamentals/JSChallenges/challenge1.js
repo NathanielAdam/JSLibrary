9e7428e22ac520e9d2d5cbb50ab271d91ad59ebf
Nathaniel Adam <grizmoore95@gmail.com>
2018-01-10 10:26:49 -0500

    JS things

---
+++ JS Fundamentals/JSChallenges/challenge1.js
@@ -0,0 +1,9 @@
+// Gold challenge
+console.log(`I have one brother`);
+console.log(`I have 4 sisters`);
+console.log(`I was born in 1995`);
+//blue challenge
+console.log(`I have ${1+4} siblings.`);
+console.log(`It has been ${2017 - 1995} years since I was born`);
+//Red Challenge
+console.log(`I have ${1+4} siblings. It has been ${2017 - 1995} years since I was born.`);
